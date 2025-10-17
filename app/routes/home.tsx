34dceed510548a77d81d69909149aaefc051527c
DeshanSamarathunga2001 <deshansamarathunga13@gmail.com>
2025-10-17 19:02:39 +0530

    Add Navbar, resume constants, and types
    Introduced a Navbar component and integrated it into the home route. Added a constant file with sample resume data and AI feedback format, as well as utility instructions for AI analysis. Defined TypeScript interfaces for Job, Resume, and Feedback to standardize data structures.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,26 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import { resumes } from "../../constant";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart Feedback Your Dream Job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Application & Resume Rating</h1>
+        <h2>Review your submission and get AI-Powerd Feedback.</h2>
+      </div>
+    </section>
+
+    {resumes}
+
+  </main>
 }
