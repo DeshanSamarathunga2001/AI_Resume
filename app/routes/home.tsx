a9371e02d665f61d110e0e5534ea450cbb15f28b
DeshanSamarathunga2001 <deshansamarathunga13@gmail.com>
2025-10-17 22:33:51 +0530

    Add ResumeCard and ScoreCircle components
    Introduces ResumeCard and ScoreCircle components for displaying resume information and scores. Updates home page to render resumes using ResumeCard, refines CSS for layout, and adjusts resume image and PDF paths in constants.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,10 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+//import { Welcome } from "../welcome/welcome";
 import Navbar from "~/components/Navbar";
 import { resumes } from "../../constant";
+import ResumeCard from "~/components/ResumeCard";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "Resumind" },
     { name: "description", content: "Smart Feedback Your Dream Job!" },
@@ -12,15 +13,23 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar/>
-    <section className="main-section">
+    <Navbar />
+    <section className="main-section py-10  ">
       <div className="page-heading">
         <h1>Track Your Application & Resume Rating</h1>
         <h2>Review your submission and get AI-Powerd Feedback.</h2>
       </div>
+
+      {resumes.length > 0 && (
+      <div className="resumes-section flex flex-wrap justify-center items-start gap-8 w-full max-w-6xl mx-auto">
+        {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume} />
+        ))}
+      </div>
+    )}
     </section>
 
-    {resumes}
+    
 
   </main>
 }
